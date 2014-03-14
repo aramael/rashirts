5ca314c0c3e930da5d712d9113be2dbfb4dc699f
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2014-03-13 22:10:11 -0400

    Added Front-End Basic UI

--- rashirtme/views.py
+++ rashirtme/views.py
@@ -4,11 +4,23 @@ Django views for rashirtme project.
 """
 
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 
 def home(request):
     """    Display the Landing Page    """
 
-    context = {}
+    context = {
+        'stripe_publishable_key': 'pk_test_qZ3TjohtOSXEeXhPubJgY64y',
+        'shirt_price': '20',
+    }
 
-    return render(request, '', context)
+    return render(request, 'home.html', context)
+
+
+def process_order(request):
+    """    Process the Incoming Order    """
+
+    print request.POST
+
+    return HttpResponse('Working!')
