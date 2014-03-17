5bfbfefd2a13e258740523598b9c3e1e4d1e0699
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2014-03-17 01:13:56 +0000

    Added one last alma shot

--- static/js/animation.js
+++ static/js/animation.js
@@ -7,6 +7,7 @@ $(function() {
     { url: 'alma_day.jpg', left: '33%', right: '33%' },
     { url: 'alma_wide_angle_low_day.jpg', left: '33%', right: '33%' },
     { url: 'alma_close_up.jpg', left: '33%', right: '33%' },
+    { url: 'alma_low_day.jpg', left: '33%', right: '33%' }
   ];
 
   var userSelected = false;
