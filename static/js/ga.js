c897f3e5606fee554ab8a1f085d2df4e5aa13f97
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2014-03-18 23:27:24 +0000

    Added Correct Google Analytics Information

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,19 +1,7 @@
-// Google Analytics
-(function(window, document, undefined) {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'XXX']);
-  _gaq.push(['_setDomainName', 'XXX']);
-  _gaq.push(['_trackPageview', document.location.pathname +
-    document.location.search + document.location.hash]);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  (function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' == document.location.protocol ?
-      'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-  })();
-})(this, this.document);
+ga('create', 'UA-7018181-11', 'rashirts.me');
+ga('send', 'pageview');
